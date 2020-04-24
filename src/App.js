f6640cfe16e1bb6415e9327dc9a90b202eff9963
raulrequenac <raulrequec@gmail.com>
2020-04-24 11:45:35 +0200

    logo & arrow

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <img className="logo puff-in-center slide-bck-tl" alt="" src="/images/logo-wd.png"></img>
+      <img className="logo puff-in-center" alt="" src="/images/logo-wd.png"></img>
+      <a href="#profile" className="scroll-down">
+        <img className="arrow slide-top" alt="" src="/images/arrow.svg"/>
+      </a>
     </div>
   );
 }
